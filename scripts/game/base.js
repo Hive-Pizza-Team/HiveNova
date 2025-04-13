820def0366e3a53fbdda94efc933c6c3d08d782d
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2025-04-12 17:34:24 -0700

    better check for keychain undefined

--- scripts/game/base.js
+++ scripts/game/base.js
@@ -268,7 +268,7 @@ $(function() {
 });
 
 const HiveKeychainLogin = async () => {
-	if (!hive_keychain) {
+	if (typeof(hive_keychain) != "undefined") {
 		alert('You must install HiveKeychain extension first');
 		return;
 	}
@@ -298,7 +298,7 @@ const HiveKeychainLogin = async () => {
 }
 
 const DepositPizzaTokens = async (hiveaccount) => {
-	if (!hive_keychain) {
+	if (typeof(hive_keychain) != "undefined") {
 		alert('You must install HiveKeychain extension first');
 		return;
 	}
