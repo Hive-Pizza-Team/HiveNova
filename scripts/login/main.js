b92223087ce70ce5247b64e232124c97b75d36f3
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2025-04-18 16:22:28 -0700

    hook up hive login button

--- scripts/login/main.js
+++ scripts/login/main.js
@@ -62,3 +62,39 @@ var Login = {
 			document.location.href = document.location.href+Query;
 	}
 };
+
+const HiveKeychainLogin = async () => {
+	if (typeof(hive_keychain) == "undefined") {
+		alert('You must install Hive Keychain extension first.');
+		return;
+	}
+
+	if (document.querySelector('#loginHive > input#username').value.length = 0 || document.querySelector('#loginHive > input#username').value.length > 16) {
+		alert('You must enter a valid Hive account name first.');
+		return;
+	}
+
+	const hiveaccount = document.querySelector('#loginHive > input#username').value;
+
+	try
+  	{
+		await hive_keychain.requestSignBuffer(
+			hiveaccount,
+			`${hiveaccount} is my account.`,
+			"Posting",
+			(response) => {
+				if (response.success) {
+					document.querySelector('input#hiveAccount').value = hiveaccount;
+					document.querySelector('input#hivesign').value = response.result;
+					document.getElementById('loginHive').submit();
+				} else {
+					console.error('Keychain error', response.error);
+				}
+			},
+			null,
+			'Moon Login'
+		);
+	} catch (error) {
+		console.error({ error });
+	}
+}
