e2b3f8f9d3def9bb3b44bb1e180cbd7a362b239d
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2025-04-24 14:22:55 -0700

    allow own name change if hive_account exists

--- includes/pages/game/ShowSettingsPage.class.php
+++ includes/pages/game/ShowSettingsPage.class.php
@@ -272,11 +272,12 @@ class ShowSettingsPage extends AbstractGamePage
 				$sql = "SELECT
 					(SELECT COUNT(*) FROM %%USERS%% WHERE universe = :universe AND username = :username) +
 					(SELECT COUNT(*) FROM %%USERS_VALID%% WHERE universe = :universe AND username = :username) +
-					(SELECT COUNT(*) FROM %%USERS_VALID%% WHERE universe = :universe AND hive_account = :username)
+					(SELECT COUNT(*) FROM %%USERS_VALID%% WHERE universe = :universe AND hive_account = :username AND id != :userid)
 				AS count";
 				$Count = $db->selectSingle($sql, array(
 					':universe'	=> Universe::current(),
-					':username'	=> $username
+					':username'	=> $username,
+					':userid' => $USER['id']
 				), 'count');
 
 				if (!empty($Count)) {
