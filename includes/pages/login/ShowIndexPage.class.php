a68a321ea251038694ede4026fa0a5c028c8e042
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2025-05-06 21:02:58 -0700

    try redirect and popup

--- includes/pages/login/ShowIndexPage.class.php
+++ includes/pages/login/ShowIndexPage.class.php
@@ -66,7 +66,7 @@ class ShowIndexPage extends AbstractLoginPage
 		if ($loginErrorMessage) {
 			AbstractLoginPage::printMessage($loginErrorMessage, array(array(
 				'label'	=> $LNG['sys_back'],
-				'url'	=> 'index.php')));
+				'url'	=> 'index.php')), array('index.php', 5), false);
 		}
 		
 		$this->display('page.index.default.tpl');
