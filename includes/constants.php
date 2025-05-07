3670e95e92fa97874007f32decb51a3679630043
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2025-05-07 14:39:38 -0700

    7 days session

--- includes/constants.php
+++ includes/constants.php
@@ -106,7 +106,7 @@ define('MAX_ATTACK_ROUNDS'			, 6);
 define('ENABLE_SIMULATOR_LINK'		, true);
 
 // Max. User Session in Seconds
-define('SESSION_LIFETIME'			, 43200);
+define('SESSION_LIFETIME'			, 604800);
 
 // ENABLE Mutlialert on sending fleets
 define('ENABLE_MULTIALERT'			, false);
