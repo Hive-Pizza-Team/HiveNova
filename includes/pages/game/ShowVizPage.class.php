856c2d81ff57c51fdd24078d1dc6c4bc4f6a47e9
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2025-07-26 13:45:39 -0700

    random offset applied to system numbers for obfuscation

--- includes/pages/game/ShowVizPage.class.php
+++ includes/pages/game/ShowVizPage.class.php
@@ -16,27 +16,48 @@
 
 class ShowVizPage extends AbstractGamePage
 {
-    function __construct() 
-	{
-		parent::__construct();
-	}
+    function __construct()
+    {
+        parent::__construct();
+    }
 
     public function show()
-	{ // startGroup,startCircle,startPoint,endGroup,endCircle,endPoint,duration,color
+    {
+        global $USER;
+        $config    = Config::get($USER['universe']);
+
+        // startGroup,startCircle,startPoint,endGroup,endCircle,endPoint,duration,color
         $fleetData = Database::get()->select(
-			'SELECT fleet_start_galaxy as startGroup, 
-            fleet_start_system as startCircle,
+            'SELECT fleet_start_galaxy as startGroup, 
+            --fleet_start_system as startCircle,
+            CASE
+                WHEN fleet_start_system < 6 THEN fleet_start_system + FLOOR(RAND() * 6)
+                WHEN fleet_start_system > :maxSystem - 6 THEN fleet_start_system - FLOOR(RAND() * 6)
+                ELSE fleet_start_system + FLOOR(RAND() * 11) - 5
+            END AS startCircle,
+
             fleet_start_planet as startPoint,
             fleet_end_galaxy as endGroup,
-            fleet_end_system as endCircle,
+
+            CASE
+                WHEN fleet_end_system < 6 THEN fleet_end_system + FLOOR(RAND() * 6)
+                WHEN fleet_end_system > :maxSystem - 6 THEN fleet_end_system - FLOOR(RAND() * 6)
+                ELSE fleet_end_system + FLOOR(RAND() * 11) - 5
+            END AS endCircle,
+
             fleet_end_planet as endPoint,
             (fleet_end_time - fleet_start_time)/100 as duration
-			FROM %%FLEETS%%'
-		);
+			FROM %%FLEETS%%',
+            array(
+                #':maxGalaxy'    => $config->max_galaxy,
+                ':maxSystem'    => $config->max_system,
+                #':maxPlanets'   => $config->max_planets
+            )
+        );
         $fleetsJson = json_encode($fleetData);
 
         $this->assign(array(
-            'fleetsJson'				=> $fleetsJson
+            'fleetsJson'                => $fleetsJson
         ));
 
         $this->display('page.viz.default.tpl');
