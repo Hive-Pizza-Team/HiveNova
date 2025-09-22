e75012dceebaaff616fa14c76893fbb1f6983405
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2025-09-22 08:01:23 -0700

    missing comma

--- install/migrations/migration_10.sql
+++ install/migrations/migration_10.sql
@@ -5,10 +5,10 @@ CREATE TABLE `%PREFIX%dm_transactions` (
   `amount_spent` float DEFAULT NULL,
   `amount_received` float DEFAULT NULL,
   `item_purchased_id` int(11) DEFAULT NULL,
-  `memo` varchar(1024) DEFAULT ''
+  `memo` varchar(1024) DEFAULT '',
   PRIMARY KEY (`id`),
 ) ENGINE=InnoDB AUTO_INCREMENT=748 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
 
 -- Upgrade sucess, set new dbVersion
 -- this is done automatically by PHP code
--- INSERT INTO %PREFIX%system SET dbVersion = 9;
+-- INSERT INTO %PREFIX%system SET dbVersion = 10;
