765f35c7c51ad14abf2ee5fda1d6be6ed94eadc1
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2025-09-22 07:57:38 -0700

    fix default value

--- install/migrations/migration_10.sql
+++ install/migrations/migration_10.sql
@@ -5,10 +5,9 @@ CREATE TABLE `%PREFIX%dm_transactions` (
   `amount_spent` float DEFAULT NULL,
   `amount_received` float DEFAULT NULL,
   `item_purchased_id` int(11) DEFAULT NULL,
-  `memo` varchar(1024) DEFAULT NULL
+  `memo` varchar(1024) DEFAULT ''
   PRIMARY KEY (`id`),
-) ENGINE=InnoDB AUTO_INCREMENT=748 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci |
-
+) ENGINE=InnoDB AUTO_INCREMENT=748 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
 
 -- Upgrade sucess, set new dbVersion
 -- this is done automatically by PHP code
