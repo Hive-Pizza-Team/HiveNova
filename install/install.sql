09cbc975a5e3f271d561b3e1fa81e9b02a30c625
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2025-09-22 08:18:05 -0700

    fix commas

--- install/install.sql
+++ install/install.sql
@@ -1400,6 +1400,6 @@ CREATE TABLE `%PREFIX%dm_transactions` (
   `amount_spent` float DEFAULT NULL,
   `amount_received` float DEFAULT NULL,
   `item_purchased_id` int(11) DEFAULT NULL,
-  `memo` varchar(1024) DEFAULT NULL
-  PRIMARY KEY (`id`),
-) ENGINE=InnoDB AUTO_INCREMENT=748 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci |
+  `memo` varchar(1024) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
