5a15242c3cb2ef473a4b69447c5c426836192200
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2026-03-08 09:32:59 -0700

    opbe fix 3

--- includes/libs/opbe/utils/Events.php
+++ includes/libs/opbe/utils/Events.php
@@ -32,7 +32,7 @@ abstract class Events
     {
         $SizeMin = MOON_MIN_START_SIZE + ($moonProb * MOON_MIN_FACTOR);
         $SizeMax = MOON_MAX_START_SIZE + ($moonProb * MOON_MAX_FACTOR);
-        $size = rand($SizeMin, $SizeMax);
+        $size = rand((int)$SizeMin, (int)$SizeMax);
         $fields = floor(pow($size / 1000, 2));
         return array('size' => $size, 'fields' => $fields);
     }
