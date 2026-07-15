134eb7d2bb9d2cd0e8faf95a04c0c995e645892e
hivetrending <62359919+hiveuprss@users.noreply.github.com>
2026-07-15 14:14:33 -0700

    Fix PHP 8.5 deprecation of PDO::MYSQL_ATTR_* constants. (#228)
    Use Pdo\Mysql::ATTR_* on PHP 8.4+ while falling back for 8.3 CI.

--- migrate.php
+++ migrate.php
@@ -31,11 +31,15 @@ require 'vendor/autoload.php';
 use HiveNova\Core\Migrator;
 
 try {
+    // PHP 8.5+ deprecates PDO::MYSQL_ATTR_*; Pdo\Mysql exists since 8.4
+    $mysqlInitCommand = class_exists(\Pdo\Mysql::class)
+        ? \Pdo\Mysql::ATTR_INIT_COMMAND
+        : PDO::MYSQL_ATTR_INIT_COMMAND;
     $pdo = new PDO(
         "mysql:host={$database['host']};port={$database['port']};dbname={$database['databasename']}",
         $database['user'],
         $database['userpw'],
-        [PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET utf8mb4, NAMES utf8mb4, sql_mode = 'STRICT_ALL_TABLES'"]
+        [$mysqlInitCommand => "SET CHARACTER SET utf8mb4, NAMES utf8mb4, sql_mode = 'STRICT_ALL_TABLES'"]
     );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
