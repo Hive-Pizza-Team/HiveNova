99f8b61f803ae091d8d4ceda8f57d4adf5293077
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2026-08-26 19:22:57 -0700

    refactor: unify ban list fetching for login and game pages (#336)
    Extract shared BanListData so both ShowBanListPage implementations use one query and formatting path.

--- includes/pages/login/ShowBanListPage.php
+++ includes/pages/login/ShowBanListPage.php
@@ -2,7 +2,7 @@
 
 namespace HiveNova\Page\Login;
 
-use HiveNova\Core\Database;
+use HiveNova\Core\BanListData;
 use HiveNova\Core\Config;
 use HiveNova\Core\HTTP;
 use HiveNova\Core\Universe;
@@ -35,48 +35,23 @@ class ShowBanListPage extends AbstractLoginPage
 	{		
 		global $LNG;
 
-		$db = Database::get();
-
-		$page  		= HTTP::_GP('side', 1);
-
-		$sql = "SELECT COUNT(*) as count FROM %%BANNED%% WHERE universe = :universe ORDER BY time DESC;";
-		$banCount = $db->selectSingle($sql, array(
-			':universe'	=> Universe::current(),
-		), 'count');
-
-		$maxPage	= ceil($banCount / BANNED_USERS_PER_PAGE);
-		$page		= max(1, min($page, $maxPage));
-		
-		$sql = "SELECT * FROM %%BANNED%% WHERE universe = :universe ORDER BY time DESC LIMIT :offset, :limit;";
-		$banResult = $db->select($sql, array(
-			':universe'	=> Universe::current(),
-			':offset'	=> (($page - 1) * BANNED_USERS_PER_PAGE),
-			':limit'	=> BANNED_USERS_PER_PAGE
-		));
-
-		$banList	= array();
-		
-		foreach($banResult as $banRow)
-		{
-			$banList[]	= array(
-				'player'	=> $banRow['who'],
-				'theme'		=> $banRow['theme'],
-				'from'		=> _date($LNG['php_tdformat'], $banRow['time'], Config::get()->timezone),
-				'to'		=> _date($LNG['php_tdformat'], $banRow['longer'], Config::get()->timezone),
-				'admin'		=> $banRow['author'],
-				'mail'		=> $banRow['email'],
-				'info'		=> sprintf($LNG['bn_writemail'], $banRow['author']),
-			);
-		}
+		$page = HTTP::_GP('side', 1);
+		$banData = BanListData::fetch(
+			Universe::current(),
+			$page,
+			$LNG['php_tdformat'],
+			Config::get()->timezone,
+			$LNG['bn_writemail']
+		);
 
 		$universeSelect	= $this->getUniverseSelector();
 		
 		$this->assign(array(
 			'universeSelect'	=> $universeSelect,
-			'banList'			=> $banList,
-			'banCount'			=> $banCount,
-			'page'				=> $page,
-			'maxPage'			=> $maxPage,
+			'banList'			=> $banData['banList'],
+			'banCount'			=> $banData['banCount'],
+			'page'				=> $banData['page'],
+			'maxPage'			=> $banData['maxPage'],
 		));
 		
 		$this->display('page.banList.default.tpl');
